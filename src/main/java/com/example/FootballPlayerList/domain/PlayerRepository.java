f4d87c2e9e00f07b97f77caa8135d47b8e26ee29
Paantee <teemu-paananen@hotmail.com>
2022-11-10 13:59:17 +0200

    error page

--- src/main/java/com/example/FootballPlayerList/domain/PlayerRepository.java
+++ src/main/java/com/example/FootballPlayerList/domain/PlayerRepository.java
@@ -1,7 +1,9 @@
 package com.example.FootballPlayerList.domain;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface PlayerRepository extends CrudRepository<Player, Long> {
-
+	
 }
